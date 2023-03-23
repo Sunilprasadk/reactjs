5305469575d8a310177305fff14f40c7c0ac9644
Sunilprasad Kuppusamy <sunilprasadk2001@gmail.com>
2023-03-23 16:00:05 +0530

    added counter incrment

--- reduxpractice/src/App.js
+++ reduxpractice/src/App.js
@@ -1,9 +1,23 @@
+import React from 'react';
+import { incrementAction } from './index';
+
+import { useSelector, useDispatch } from 'react-redux';
+
 function App() {
+
+  const counter = useSelector(state=>state.count);
+  const dispatch = useDispatch();
+
+  function incrementHandler(){
+    dispatch(incrementAction);
+  }
 
   return (
     <>
+    <h2>Count : {counter}</h2>
+    <button onClick={incrementHandler}>+</button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- reduxpractice/src/index.js
+++ reduxpractice/src/index.js
@@ -2,7 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import {legacy_createStore as createStore} from 'redux';
+
+import { Provider } from 'react-redux';
+
+const counterReducer = (state={
+  count:0
+}, action)=>{
+  if(action.type==='INCREMENT'){
+    return {
+      ...state,
+      count : state.count+1
+    }
+  }else{
+    return state;
+  }
+}
+
+export const incrementAction = {
+  type: 'INCREMENT'
+}
+
+const store = createStore(counterReducer);
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <Provider store= {store}>
+    <App />
+  </Provider>
 );
