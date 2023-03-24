6f1199651e07fe6d417291b3302ac7d7591ea95a
Sunilprasad Kuppusamy <sunilprasadk2001@gmail.com>
2023-03-24 10:44:51 +0530

    array rendering using useState done !

--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  function buttonHandler(){
+    setData([...data, "Sunil"]);
+  }
+
   return (
     <div className="App">
-      
+      <h2>Hello !</h2>
+      {data.map((ele, indx)=>{
+        return <i key={indx}>{ele}</i>})
+      }
+      <button onClick={buttonHandler}>Add Me</button>
     </div>
   );
 }
